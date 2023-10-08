60a84e4a6a2345fc072be51f0c90a5d5eaed197b
Stiwarth Vega <38712164+ssvef@users.noreply.github.com>
2023-10-08 16:46:22 -0500

    Update test1.dart

--- test1.dart
+++ test1.dart
@@ -1,61 +1,145 @@
+// Integrantes: Castrillon Camilo, Vega Stiwarth
 import "dart:io";
-void main() {
-  bool salir = false;
-  while (!salir){
-    print("Menu principal gestor de inventarios\n por favor seleccione la opcion que decea con su respectivo numero\n1.menu gestion de productos(agregar,buscar y eliminar)\n2.mostrar inventario\n3.realizar venta\n4.mostrar ganancias totales\n5.salir");
-    int opciones1=int.parse(stdin.readLineSync()!);
-      if (opciones1==1){
-        print("por favor seleccione la opccion deseada\n1.agregar peluchito\n2.buscar peluchito\n3.eliminar peluchito\n4.salir\n5.volver atras");
-        int opciones2=int.parse(stdin.readLineSync()!);
-      if(opciones2==1){
-          Map<String,String>agregar={};
-          bool nomas= false;
-          double valortotal=0.0;
-          while(!nomas){
-          print("por favor ingrese el nombre del peluche(compuesto de una sola palabra) y su cantidad");
-          print("ingrese el nombre del pelcuhce");
-          String nombrepeluche= stdin.readLineSync()!;
-          print("ingrese la cantidad de peluches $nombrepeluche");
-          String cantidadpeluches= stdin.readLineSync()!;
-         print("cual es el precio individual de $nombrepeluche?");
-          String valorpeluche= stdin.readLineSync()!;
-             agregar[nombrepeluche]= cantidadpeluches+"/"+valorpeluche;
-          print("decea agregar mas peluches?\n1.si\n2.no");
-          int agregarmas= int.parse(stdin.readLineSync()!);
-          if(agregarmas==1){
-          print("está bien");
-          }else if(agregarmas==2|| agregarmas>2){
-          nomas=true;
-            //verificar la separacion del valor de la llave
-            //operacion de separacion
-          print("lista de peluches");
-          agregar.forEach((key, value) {
-          print("$key, cantidad: $value");
-          },);
-          print("Valor total de todos los peluches: \$${valortotal.toStringAsFixed(2)}");
-          }   
-            print(agregar); // testing stiwarth
-          }}else if (opciones2==2){
-          //TODO LO RELACIONADO A BUSCAR PELUCHITO
-        }else if(opciones2==3){
-          //TODO LO RELACIONADO A ELIMINAR PELUCHE 
-        }else if (opciones2<1||opciones2>5){
-          print("numero invalido, por favor ingrese un numero del 1 al 5");
-        }else if(opciones2==5){
-          continue;
-        }else if(opciones2==4){
-          print("gracias por su tiempo, hasta luego");
-          salir=true;}
-      }if (opciones1==2){
-      print("mama tengo hambre");
-      //TODO LO RELACIONADO CON MOSTRAR INVENTARIO
-    }if (opciones1==3){
-      //TODO LO RELACIONADO A REALIZAR VENTA
-    }if (opciones1==4){
-      //TODO LO RELACIONADO A MOSTRAR GANANCIAS TOTALES
-    }if (opciones1==5){
-      print("gracias por su tiempo hasta luego");  
-      salir=true;
-    } 
+List<List<dynamic>> inventory = [];
+//main menu
+void main(){
+  int option;
+  do{ 
+  print("\nMenu principal gestor de inventarios\nPor favor seleccione acción a realizar\n\n1.Agregar\n2.Buscar\n3.Eliminar\n4.Mostrar inventario\n5.Realizar venta\n6.Mostrar ganancias totales\n7.Salir\n");
+ option = int.parse(stdin.readLineSync()!);
+    switch (option){
+      case 1:
+      addplushtoy();
+        break;
+      case 2: 
+        lookforplushtoy();
+        break;
+      case 3:
+        deleteplushtoy();
+        break;
+      case 4:
+        mostrarinventario();
+        break;
+      case 5:
+       realizarventa();
+        break;
+      case 6:
+        mostrargananciastotales();
+        break;
+      case 7:
+        print("\nEl programa se cerrará. Gracias por su tiempo"); 
+        break;
+      default:
+        print("\nOpcion invalida intente de nuevo por favor.\n");
+        break;
+    }
+  }while (option != 7);  
+}
+
+void addplushtoy() {
+  String toyname;
+  int toyamount, toyprice,option;
+  do{
+  print("\nA continuación debe ingresar los siguientes datos para registrar el peluche");
+  print("Ingrese el nombre del peluche");
+     toyname= stdin.readLineSync()!;
+  print("\nIngrese la cantidad en stock para el peluche \"$toyname\"");
+    toyamount= int.parse(stdin.readLineSync()!);
+  print("\nIngrese el valor unitario para el peluche \"$toyname\"");
+     toyprice= int.parse(stdin.readLineSync()!); 
+  List <dynamic>plushtoy= [toyname, toyamount,toyprice,1];//creacion de lista para guardar los peluches, dinamica, la estatica es inventory.
+  inventory.add (plushtoy);
+  for(var plushtoy in inventory ){
+    if (plushtoy[0] == toyname) {
+    print("\nEl peluche $toyname fue registrado correctamente.\n");
+  }else{
+      print("Ocurrio un error en el registro, intente nuevamente.");
   }
-} 
+}
+    print("¿Desea añadir un nuevo peluche? Responda 1 para Si o 2 para No\n Si responde que no regresará al menu principal");
+    option=int.parse(stdin.readLineSync()!);
+  }while(option==1);
+}
+
+void lookforplushtoy(){
+  String toyname;
+  int option=1;
+  do{
+    print("\nIngrese el nombre del peluche a consultar");
+    toyname = stdin.readLineSync()!;
+  print("\nLISTA DE PELUCHES EN STOCK");
+   for(var plushtoy in inventory ){
+    if (plushtoy[0] == toyname) {
+   print("Se encontró la siguiente existencia:\n\nNombre: ${plushtoy[0]}, Cantidad: ${plushtoy[1]}, Valor: \$${plushtoy[2]} pesos\n\n¿Desea generar una nueva consulta?Responda 1 para Si o 2 para No\n Si responde que no regresará al menu principal");
+      option=int.parse(stdin.readLineSync()!);
+    }else{
+      print ("El nombre $toyname no existe en el inventario.\n¿Desea generar una nueva consulta?Responda 1 para Si o 2 para No\n Si responde que no regresará al menu principal");
+      option=int.parse(stdin.readLineSync()!);
+    }
+  }
+}while(option==1);
+}
+
+void deleteplushtoy() {
+  
+  int option=1,warning;
+  String toyname;
+  do{
+    print("********!ALERTA¡********\n\nAcaba de ingresar al menú de eliminación tenga presente que este menú tiene dos opciones de eliminación.\nLa primera opcion consiste en eliminar el producto dejando un registro de existencia para auditoria.\nEl segundo tipo de eliminación es una eliminacion definitiva, no quedará registro de la existencia del producto.\nA continuación digite el numero 1 si comprende esta información para prodeceder al menu de opciones.\n");
+    warning=int.parse(stdin.readLineSync()!);
+  }while(warning!=1);
+  do{
+print("*****MENU DE ELIMINACION DE PELUCHES.*****\n");
+if(inventory.isEmpty==true){
+    print ("Actualmente no hay productos en stock.\nRecomendamos agregar nuevos peluches.\nIngrese un numero diferente a 1 para regresar al menu principal.");
+      option=int.parse(stdin.readLineSync()!);
+}else{
+  print("\nIngrese el nombre del peluche a eliminar:\n");
+    toyname = stdin.readLineSync()!;
+   for (var plushtoy in inventory) { 
+       if(plushtoy[0] == toyname){ 
+        inventory.remove(plushtoy);
+          print("Peluche eliminado.\n¿Desea generar una nueva consulta?Responda 1 para Si o 2 para No.Si responde que no regresará al menu principal");
+         option=int.parse(stdin.readLineSync()!);
+   }else{
+         print ("\nEl nombre $toyname no existe en el inventario.\n\n¿Desea generar una nueva consulta para eliminar el peluche?Responda 1 para Si o 2 para No\n Si responde que no regresará al menu principal");
+	option=int.parse(stdin.readLineSync()!);} 
+  } 
+}
+  }while(option!=1);
+}
+ void mostrarinventario(){
+for (var plushtoy in inventory){
+print("el inventario completo es:${plushtoy[0]}, Cantidad: ${plushtoy[1]}, Valor: \$${plushtoy[2]}");
+}
+ }
+
+void realizarventa() {
+  print("Ingrese el nombre del peluche:");
+  String nombre = stdin.readLineSync()!;
+  bool encontrado = false;
+  for (var plushtoy in inventory) {
+    if (plushtoy[0] == nombre) {
+      print("Ingrese la cantidad a vender:");
+      int cantidadventa = int.parse(stdin.readLineSync()!);
+      if (cantidadventa <= plushtoy[1]) {
+        plushtoy[1] -= cantidadventa;
+        print("Venta realizada.");
+      } else {
+        print("No hay suficientes peluches en inventario.");
+      }encontrado = true;
+      break;
+    }
+  }if (!encontrado){
+  }
+}
+void mostrargananciastotales() {
+  double gananciastotales= 0.0;
+  for (var plushtoy in inventory){
+  double gananciassubtotales= plushtoy[2]*plushtoy[1];
+print("las ganancias totales por peluche son: nombre:${plushtoy[0]} ventas ${plushtoy[1]} ganancias $gananciassubtotales");
+  gananciastotales+=gananciassubtotales;
+  }
+  
+print("las ganancias totales son $gananciastotales");
+}
